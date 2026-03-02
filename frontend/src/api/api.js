796981a1b35cf7c4122eb55c572c944583bdd5a0
Aman Verma <amanvrma798@gmail.com>
2026-03-02 15:53:51 +0530

    Updated API baseURL for production

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://hrms-backend-0r61.onrender.com/api/",
 });
 
 API.interceptors.response.use(
